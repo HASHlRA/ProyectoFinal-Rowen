e75a3dd477a9ec46ec4b6bfbe7d01b5b322151e5
Rowen Lumanlan <rowenlumanlan@gmail.com>
2022-11-30 11:51:49 +0100

    ScoreText

--- Assets/Scripts/RL_Obstacle.cs
+++ Assets/Scripts/RL_Obstacle.cs
@@ -1,13 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RL_Obstacle : MonoBehaviour
 {
+    private GameObject player;
+    public static RL_Obstacle Instance;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void sceneToMoveTo()
+    {
+        SceneManager.LoadScene("RL_GameOver");
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+
+        else if (Instance != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -15,6 +37,12 @@ public class RL_Obstacle : MonoBehaviour
         if(collision.tag == "Border")
         {
             Destroy(this.gameObject);
+        }
+
+        else if(collision.tag == "Player")
+        {
+            Destroy(player.gameObject);
+            RL_Obstacle.Instance.sceneToMoveTo();
         }
     }
 }
